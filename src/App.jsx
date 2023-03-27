025b5fec9331d3058b5906e672712d72111f9d1d
Afra <propazaman12@gmail.com>
2023-03-27 11:24:33 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Order from './components/Order/Order'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,8 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Order></Order>
+    
     </div>
   )
 }
---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Order.css';
+
+const Order = () => {
+    return (
+        <div className='order-container'>
+            <div className="products-container">
+                <h2>Products</h2>
+
+            </div>
+
+            <div className="cart-container">
+                <h2>Order summary</h2>
+
+            </div>
+        </div>
+    );
+};
+
+export default Order;
