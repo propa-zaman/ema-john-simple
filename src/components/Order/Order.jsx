6dd34c33116055539cad60970a15451917ba3127
Afra <propazaman12@gmail.com>
2023-03-30 02:37:49 +0600

    retrive from local storage

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -19,7 +19,27 @@ const Order = () => {
 
     useEffect(() =>{
         const storedCart = getShoppingCart();
-    },[])
+        const savedCart = [];
+        // step 1: get id
+        for(const id in storedCart){
+            // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the savedCart
+                savedCart.push(addedProduct);
+            }
+            console.log(addedProduct);
+            // step 3: get quantity of the product
+            
+
+
+        }
+        // step 5: set to setCart
+        setCart(savedCart);
+    },[products])
 
     const handleAddToCart = (product) => {
         // cart.push(product)
